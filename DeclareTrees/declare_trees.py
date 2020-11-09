d2a648523e524248712e2b9a3f56e44e59c7b6af
Alessio Cecconi <alessio.cecconi.1991@gmail.com>
2020-11-09 14:39:20 +0100

    dynamic declare trees

--- DeclareTrees/declare_trees.py
+++ DeclareTrees/declare_trees.py
@@ -14,10 +14,10 @@ class ClusterNode:
         self.nok = None  # child node not fulfilling the constraint
         self.constraint = constraint  # Constraint discriminating the current node
         self.threshold = threshold  # Constraint threshold discriminating the current node
-        self.clusters = set()  # Set of cluster at th current node
+        self.clusters = set()  # Set of cluster at the current node
+        self.used_constraints = set()  #
 
-    def insert_child(self, cluster_name, cluster_data):
-        value = cluster_data[self.constraint]
+    def insert_child(self, cluster_name, value):
         if value >= self.threshold:
             if not self.ok:
                 self.ok = ClusterNode(threshold=self.threshold)
@@ -73,9 +73,9 @@ def print_tree_graphviz(graph, node):
     return this_node_code
 
 
-def order_constraints(clusters_file):
+def order_constraints_overall(clusters_file):
     """
-    It orders the constraints from the most common across the cluster to the less one
+    It orders the constraints from the most common across the cluster to the less one from the SJ2T results
     :param clusters:
     """
     priority_sorted_constraints = []
@@ -111,13 +111,14 @@ def order_constraints(clusters_file):
 
 def build_declare_tree(clusters_file, threshold, output_file):
     """
-Builds the DECLARE tree according to the aggregated result of the clusters
+Builds the DECLARE tree according to the aggregated result of the clusters.
+Constraints are used in total frequency order from the most common among the clusters to the rarest one
     :param output_file:
     :param clusters_file:
     :param threshold:
     :return:
     """
-    ordered_constraints, constraints_map, clusters_map = order_constraints(clusters_file)
+    ordered_constraints, constraints_map, clusters_map = order_constraints_overall(clusters_file)
     # root
     result_tree = ClusterNode(threshold=threshold)
     result_tree.clusters = set(clusters_map.keys())
@@ -130,7 +131,7 @@ Builds the DECLARE tree according to the aggregated result of the clusters
                 continue
             leaf.constraint = constraint
             for cluster_in_node in leaf.clusters:
-                leaf.insert_child(cluster_in_node, clusters_map[cluster_in_node])
+                leaf.insert_child(cluster_in_node, clusters_map[cluster_in_node][constraint])
                 if leaf.ok:
                     new_leaves.add(leaf.ok)
                 if leaf.nok:
@@ -145,24 +146,122 @@ Builds the DECLARE tree according to the aggregated result of the clusters
     return result_tree
 
 
-# # MAIN for CLUSTERs
-# if __name__ == '__main__':
-#     print(sys.argv)
-#     clusters_file = sys.argv[1]
-#     constraints_threshold = float(sys.argv[2])
-#     output_file = sys.argv[3]
-#     build_declare_tree(clusters_file, constraints_threshold, output_file)
-#
-# #  MAIN for LOGs
-# if __name__ == '__main__':
-#     print(sys.argv)
-#     sj2t_file = sys.argv[1]
-#     constraints_threshold = float(sys.argv[2])
-#     preprocessed_file = sys.argv[3]
-#     output_file = sys.argv[4]
-#
-#     # Pre-prcessing
-#     cmio.extract_detailed_perspective(sj2t_file, preprocessed_file)
-#
-#     # Declare tree
-#     build_declare_tree(preprocessed_file, constraints_threshold, output_file)
+def get_clusters_table(clusters_file):
+    """
+    It builds a matrix from the SJ2T results
+
+    result [constraint x cluster] with headers for column and rows, plus last column "SUM" is the sum of the row
+    Thus the column 0 are the constraints names, row 0 are the clusters names, column -1 is the sum of the row
+
+    :param clusters:
+    """
+    clusters_table = []
+    clusters_index = {}
+    constraints_index = {}
+    with open(clusters_file, 'r') as aggregated_result:
+        # cluster_csv = csv.reader(aggregated_result, delimiter=';')
+        csv_map = csv.reader(aggregated_result, delimiter=';')
+        first = True
+        for line in csv_map:
+            if first:
+                row = line + ['SUM']
+                first = False
+            else:
+                row = [line[0]]
+                sum_temp = 0.0
+                for i in line[1:]:
+                    row += [float(i)]
+                    if i == 'nan':
+                        continue
+                    else:
+                        sum_temp += float(i)
+                row += [sum_temp]
+            clusters_table += [row]
+    clusters_counter = 1
+    for cluster in clusters_table[0][1:-1]:
+        clusters_index[cluster] = clusters_counter
+        clusters_counter += 1
+    constraints_counter = 1
+    for constraint in clusters_table[1:]:
+        constraints_index[constraint[0]] = constraints_counter
+        constraints_counter += 1
+    return clusters_table, clusters_index, constraints_index
+
+
+def order_clusters_table(clusters_table):
+    """
+    Given a matrix [constrain X clusters] It orders the constraints by frequency across the clusters
+    :param clusters_table:
+    """
+    clusters_table = [clusters_table[0]] + sorted(clusters_table[1:], key=lambda item: item[-1], reverse=True)
+    return clusters_table
+
+
+def get_most_common_constraint(cluster_table, clusters, used_constraints):
+    view = []
+    header = True
+    for row in cluster_table:
+        if row[0] in used_constraints:
+            continue
+        view_row = [row[0]]
+        sum_row = 0.0
+        for cluster_i in range(len(row)):
+            if cluster_table[0][cluster_i] in clusters:
+                view_row += [row[cluster_i]]
+                if header or math.isnan(row[cluster_i]):
+                    continue
+                sum_row += row[cluster_i]
+        if header:
+            view_row += [row[-1]]
+            header = False
+        else:
+            view_row += [sum_row]
+        view += [view_row]
+    return order_clusters_table(view)[1][0]
+
+
+def build_declare_tree_dynamic(clusters_file, threshold, output_file):
+    """
+Builds the DECLARE tree according to the aggregated result of the clusters.
+Constraints are reordered in each sub-branch according to the frequency in the remaining clusters.
+    :param output_file:
+    :param clusters_file:
+    :param threshold:
+    :return:
+    """
+    # Import initial data
+    clusters_table, clusters_indices, constraints_indices = get_clusters_table(clusters_file)
+    # root initialization
+    result_tree = ClusterNode(threshold=threshold)
+    result_tree.clusters = set(clusters_table[0][1:-1])
+    leaves = set()
+    leaves.add(result_tree)
+
+    # while splittable leaves
+    while len(leaves) > 0:
+        #   for branch
+        new_leaves = set()
+        for leaf in leaves:
+            if len(leaf.clusters) == 1 or len(leaf.used_constraints) == len(constraints_indices):
+                continue
+            #       split according to most frequent constraint
+            leaf.constraint = get_most_common_constraint(clusters_table, leaf.clusters, leaf.used_constraints)
+            for cluster_in_node in leaf.clusters:
+                leaf.insert_child(cluster_in_node, clusters_table[constraints_indices[leaf.constraint]][
+                    clusters_indices[cluster_in_node]])
+            if leaf.ok:
+                leaf.ok.used_constraints = leaf.used_constraints.copy()
+                leaf.ok.used_constraints.add(leaf.constraint)
+                new_leaves.add(leaf.ok)
+            if leaf.nok:
+                leaf.nok.used_constraints = leaf.used_constraints.copy()
+                leaf.nok.used_constraints.add(leaf.constraint)
+                new_leaves.add(leaf.nok)
+        leaves = new_leaves
+
+    print("### Graphviz")
+    graph = graphviz.Digraph(format='svg')
+    print_tree_graphviz(graph, result_tree)
+    graph.render(filename=output_file)
+
+    return result_tree
--- DeclareTrees/trees_for_clusters.py
+++ DeclareTrees/trees_for_clusters.py
@@ -1,9 +1,21 @@
 import sys
 from DeclareTrees.declare_trees import build_declare_tree
+from DeclareTrees.declare_trees import build_declare_tree_dynamic
 
 if __name__ == '__main__':
     print(sys.argv)
     clusters_file = sys.argv[1]
     constraints_threshold = float(sys.argv[2])
     output_file = sys.argv[3]
-    build_declare_tree(clusters_file, constraints_threshold, output_file)
+    branching_policy = sys.argv[4]  # [frequency, dynamic, minimal]
+    # frequency: total frequency among all the clusters (each level in each branch splits with the same constraint)
+    # dynamic: each split is given by the most frequent constraint among the clusters in the branch
+    # minimal: single choice splits are discarded adn the tree contains only the separating factors
+    if branching_policy == "frequency":
+        build_declare_tree(clusters_file, constraints_threshold, output_file)
+    elif branching_policy == "dynamic":
+        build_declare_tree_dynamic(clusters_file, constraints_threshold, output_file)
+    elif branching_policy == "minimal":
+        print("SYNAMIC MINIMAL NOT YET IMPLEMENTED")
+    else:
+        print("Branching policy not recognized. Supported policies: [frequency, dynamic, minimal] ")
