10c3fba8894768cfaebaf61f4cd24bdc86208429
Alessio Cecconi <alessio.cecconi.1991@gmail.com>
2021-11-12 17:39:07 +0100

    forgotten file from previous commit

---
+++ ClusteringEvaluation/label_traces_from_clustered_logs.py
@@ -0,0 +1,19 @@
+import os
+import sys
+
+from ClusteringEvaluation.utils import load_clusters_logs_from_folder, export_traces_clusters_labels
+
+
+def export_traces_clusters_labels_from_logs(folder, name="traces-labels.csv"):
+    result, clusters_indices = load_clusters_logs_from_folder(folder)
+    labels = []
+    for cluster_index, log in enumerate(result):
+        for t in log:
+            labels += [clusters_indices[cluster_index]]
+    export_traces_clusters_labels(labels, os.path.join(folder, name))
+
+
+if __name__ == '__main__':
+    print(sys.argv)
+    logs_folder = sys.argv[1]
+    export_traces_clusters_labels_from_logs(logs_folder)
